2b1ef6c7eee35a64e8348c57c8b9879c01cb7a66
Vincent Kashaka <vincekashaka@gmail.com>
2022-09-05 19:22:08 +0200

    added validation

--- src/main/java/com/vince/reactive/econet/controller/AreaController.java
+++ src/main/java/com/vince/reactive/econet/controller/AreaController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/v1/econet")
 public class AreaController {
@@ -20,23 +22,28 @@ public class AreaController {
         return areaService.getAllAreas();
     }
 
-    @GetMapping("/areas{areaId}")
+    @GetMapping("/{areaId}")
     public Mono<AreaDto> getAreaById(@PathVariable String areaId){
         return areaService.getAreaById(areaId);
     }
 
 
     @PostMapping
-    public Mono<AreaDto> saveArea(@RequestBody Mono<AreaDto> areaDtoMono){
+    public Mono<AreaDto> saveArea(
+            @Valid
+            @RequestBody Mono<AreaDto> areaDtoMono){
         return areaService.saveArea(areaDtoMono);
     }
 
     @PutMapping("/area/{areaId}")
-    public Mono<AreaDto> updateArea(@RequestBody Mono<AreaDto> areaDtoMono, @PathVariable String areaId){
+    public Mono<AreaDto> updateArea(
+            @Valid
+            @RequestBody Mono<AreaDto> areaDtoMono,
+            @PathVariable String areaId){
         return areaService.updateArea(areaDtoMono, areaId);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/delete/{areaId}")
     public Mono<Void> deleteArea(@PathVariable String areaId){
         return areaService.deleteArea(areaId);
     }
